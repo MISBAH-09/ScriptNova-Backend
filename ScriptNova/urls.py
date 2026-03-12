cc3523f61b9759b8f3eb18a19c32519dd74b1822
Your Name <you@example.com>
2026-03-12 13:44:52 +0500

    Setup URL routing and register authentication middleware

---
+++ ScriptNova/urls.py
@@ -0,0 +1,15 @@
+"""
+URL configuration for ScriptNova app.
+"""
+from django.urls import path
+from ScriptNova.views import signupAPI, loginAPI, getByIdApi, updateAPI
+
+app_name = 'ScriptNova'
+
+urlpatterns = [
+    # Authentication endpoints
+    path('signup', signupAPI.as_view(), name='signup'),
+    path('login', loginAPI.as_view(), name='login'),
+    path('user/<int:id>', getByIdApi.as_view(), name='get-user'),
+    path('user/update', updateAPI.as_view(), name='update-user'),
+]
--- ScriptNova/views/__init__.py
+++ ScriptNova/views/__init__.py
@@ -4,10 +4,6 @@ Views module for ScriptNova app.
 Import all views here for cleaner organization.
 """
 
-# from .auth import *  # Uncomment when you add auth views
-# from .products import *  # Uncomment when you add product views
-# from .orders import *  # Uncomment when you add order views
+from .Authentication import signupAPI, loginAPI, getByIdApi, updateAPI
 
-from .Authentication import *  
-
-__all__ = []
+__all__ = ['signupAPI', 'loginAPI', 'getByIdApi', 'updateAPI']
